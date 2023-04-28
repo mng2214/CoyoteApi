5698484a0debac0c42a4e2947893fa29cd362ad6
mng2214 <mng2214@gmail.com>
2023-04-28 14:18:02 -0500

    lang and lat added

---
+++ src/main/java/brokers/coyote/CoyoteAddressPojo.java
@@ -0,0 +1,9 @@
+package brokers.coyote;
+
+public class CoyoteAddressPojo {
+    private String city;
+    private String county;
+    private String state;
+    private String country;
+    private String country_code;
+}
--- src/main/java/brokers/coyote/CoyoteTest.java
+++ src/main/java/brokers/coyote/CoyoteTest.java
@@ -1,4 +1,39 @@
 package brokers.coyote;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
 public class CoyoteTest {
+    @Test
+    public void getLatLng() {
+        //String city, String state
+        RestAssured.baseURI = "https://nominatim.openstreetmap.org/";
+        RestAssured.basePath = "search";
+        Response response = RestAssured.given().accept(ContentType.JSON)
+                .queryParam("city", "Chicago")
+                .queryParam("state", "IL")
+                .queryParam("country", "USA")
+                .queryParam("format", "json")
+                .queryParam("addressdetails", 1)
+                .queryParam("limit", 1)
+                .when()
+                .get()
+                .then()
+                .statusCode(200)
+               // .log().all()
+                .extract().response();
+
+       // CoyotePojo parsedResponse = response.as(CoyotePojo.class);
+        JsonPath jsonPath = response.jsonPath();
+        List<String> lat = jsonPath.get("lat");
+        List<String> lon = jsonPath.get("lon");
+        System.out.println(lat +"\n"+lon);
+    }
 }
